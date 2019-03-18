7d45556f0435728c112c4c3f04b8796ca33e559f
Mike Baynton <mbaynton@umn.edu>
2019-03-18 13:20:17 -0500

    Fix module name in metadata.json and add a little rspec testing

---
+++ spec/classes/add_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'local_users::add' do
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      it { is_expected.to compile }
+
+      # Dependent on number of users in spec/fixtures/hiera/common.yaml
+      it { is_expected.to have_user_resource_count(1) }
+
+      # sensitive = Puppet::Pops::Types::PSensitiveType::Sensitive.new('$6$/dBBM855e2zWLTa6$YiP9qjLYyDyMiBnDRg9Buxg4xKcmOFqCx6zYbd4HaJthZ92ybpUTIu8vcZw63wvngutvD7vHjuYldIa/ktAK6/')
+      it { is_expected.to contain_user('root').with('password' => '$6$/dBBM855e2zWLTa6$YiP9qjLYyDyMiBnDRg9Buxg4xKcmOFqCx6zYbd4HaJthZ92ybpUTIu8vcZw63wvngutvD7vHjuYldIa/ktAK6/') }
+    end
+  end
+end
